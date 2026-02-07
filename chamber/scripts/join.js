86f7d77acaf981f95be92e3b81d0edfe1038c55c
NNOGS <nnogswill@gmail.com>
2026-02-07 19:17:48 +0100

    Join Page Update

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -2,5 +2,27 @@ const timestamp = document.querySelector("#timestamp");
 timestamp.value = new Date().toISOString();
 
 
+
+// Timestamp
 document.getElementById("timestamp").value = new Date().toISOString();
+
+// Open modal buttons
+const openButtons = document.querySelectorAll(".open-modal");
+
+openButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const modalId = button.dataset.modal;
+    const modal = document.getElementById(modalId);
+    modal.showModal();
+  });
+});
+
+// Close modal buttons
+const closeButtons = document.querySelectorAll(".close-modal");
+
+closeButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    button.closest("dialog").close();
+  });
+});
 
